8f7ec42981353705226cb7867f71402a2603aa35
Zach Prong <prongzachary@gmail.com>
2019-11-08 03:30:30 -0800

    use react-router-dom

--- roomies/src/App.js
+++ roomies/src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import RoomiesLanding from './RoomiesLanding';
+import RoomiesHome from './RoomiesHome';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header style={{backgroundColor: '#F9F9F9', height: '1200px'}}>
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/home">
+                <RoomiesHome/>
+              </Route>
+              <Route path="/">
+                <RoomiesLanding/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
---
+++ roomies/src/RoomiesLanding.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RoomiesLanding() {
+    return (
+      <div style={{}}>
+          <h1>Roomies</h1>
+          <p>It's time to make your Roommates Great</p>
+      </div>
+    );
+  }
+  
+  export default RoomiesLanding;
