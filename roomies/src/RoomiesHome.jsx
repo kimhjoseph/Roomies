5f0a4a0c96056d184af9a3ceed9f88d9c961e413
akpham97 <apham1109@ucla.edu>
2019-11-11 22:08:24 -0800

    user list on homepage

--- roomies/src/RoomiesHome.jsx
+++ roomies/src/RoomiesHome.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import UserList from './UserList'
 
 function RoomiesHome() {
     return (
       <div>
           <h1>Good Morning Rondald</h1>
+          <UserList/>
 
       </div>
     );
