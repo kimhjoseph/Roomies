118e8009898195f600a47ae4093628b44abc7d9c
Saman <saman.hpour@gmail.com>
2019-11-11 22:10:45 -0800

    add notification list to homepage

--- roomies/src/RoomiesHome.jsx
+++ roomies/src/RoomiesHome.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import UserList from './UserList'
+import NotificationCards from './NotificationCards'
 
 function RoomiesHome() {
     return (
       <div>
           <h1>Good Morning Rondald</h1>
           <UserList/>
+          <NotificationCards/>
 
       </div>
     );
   }
-  
+
   export default RoomiesHome;
