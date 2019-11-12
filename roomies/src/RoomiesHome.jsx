7bc38238f27eb8ca325817195c744e0e31b099c0
Saman <saman.hpour@gmail.com>
2019-11-11 20:35:21 -0800

    Notification Card Components created and added

--- roomies/src/RoomiesHome.jsx
+++ roomies/src/RoomiesHome.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import UserCard from './UserCard';
+import NotificationCard from './NotificationCard';
 
 function RoomiesHome() {
     return (
       <div>
           <h1>Good Morning Rondald</h1>
-      
+
 
       <Container>
       	<Row>
-      		<Col sm="2">
+      		<Col sm="4">
       			<UserCard />
       			<UserCard />
       			<UserCard />
@@ -18,8 +19,17 @@ function RoomiesHome() {
       	</Row>
       </Container>
 
+      <Container>
+      	<Row>
+      		<Col sm="4">
+      			<NotificationCard />
+      			<NotificationCard />
+      			<NotificationCard />
+      		</Col>
+      	</Row>
+      </Container>
       </div>
     );
   }
-  
+
   export default RoomiesHome;
