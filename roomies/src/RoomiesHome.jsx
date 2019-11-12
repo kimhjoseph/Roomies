896beeb0bec5ce8f6dab05225087100ed610a024
akpham97 <apham1109@ucla.edu>
2019-11-11 20:22:37 -0800

    user card component added

--- roomies/src/RoomiesHome.jsx
+++ roomies/src/RoomiesHome.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import UserCard from './UserCard';
 
 function RoomiesHome() {
     return (
       <div>
           <h1>Good Morning Rondald</h1>
+      
+
+      <Container>
+      	<Row>
+      		<Col sm="2">
+      			<UserCard />
+      			<UserCard />
+      			<UserCard />
+      		</Col>
+      	</Row>
+      </Container>
+
       </div>
     );
   }
---
+++ roomies/src/UserCard.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+class UserCard extends Component {
+	constructor() {
+		super();
+	}
+
+	render () {
+		return (
+		    <div>
+		      <Card>
+		        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+		        <CardBody>
+		          <CardTitle>Zachary</CardTitle>
+		          <CardSubtitle>Card subtitle</CardSubtitle>
+		          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+		          <Button>Button</Button>
+		        </CardBody>
+		      </Card>
+		    </div>
+
+		)
+	}
+}
+
+export default UserCard;
