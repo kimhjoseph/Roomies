cf6253f5e01a5dbbe2dde803aaf01f66df2abebf
akpham97 <apham1109@ucla.edu>
2019-11-26 00:51:20 -0800

    Added chores list functionality.

--- roomies/src/components/ChoreList.js
+++ roomies/src/components/ChoreList.js
@@ -1,17 +1,156 @@
 import React, { Component } from "react";
-import { Table, Card, Button, ToggleButton } from "react-bootstrap";
+import { Table, Card } from "react-bootstrap";
 import dummy from "../images/dummy.jpg";
 import "./Chores.css";
+import ChoreAddItemModal from "./ChoreModal";
+
+
 
 export default class ChoreList extends Component {
   constructor(props) {
     super(props);
+    this.click = this.click.bind(this);
+
+
+    this.showAddChoreModal = this.showAddChoreModal.bind(this);
+    this.handleAddChore = this.handleAddChore.bind(this);
+    this.updateChore = this.updateChore.bind(this);
+    this.updateDays = this.updateDays.bind(this);
+    this.updatePerson = this.updatePerson.bind(this);
+    this.handleDisableClick = this.handleDisableClick.bind(this);
+
+    this.state = {
+      addChoreModal: false,
+      chores: [
+        {
+          chore: "Dishes",
+          person: "Rondald",
+          days: "5",
+          complete: false
+        },
+        {
+          chore: "Trash",
+          person: "Audrey",
+          days: "2",
+          complete: false
+        },
+        {
+          chore: "Shopping",
+          person: "Zachary",
+          days: "3",
+          complete: false
+        },
+        {
+          chore: "Clean Bathroom",
+          person: "Rondald",
+          days: "2",
+          complete: false
+        }
+      ],
+      tempChore: {
+          chore: "",
+          person: "",
+          days: "",
+          complete: false
+      }
+    };
   }
+
+  click(item) {
+    console.log("hi");
+    console.log(item);
+    var array = this.state.chores;
+    var index = array.indexOf(item);
+    if (index !== -1) {
+      array.splice(index, 1);
+      this.setState({
+        chores: array
+      });
+  }
+  console.log(array);
+}
+
+  showAddChoreModal() {
+    this.setState({ addChoreModal: !this.state.addChoreModal });
+  }
+
+  handleAddChore(tempChore) {
+    console.log("called handleChoreItem");
+    console.log(tempChore);
+    this.setState(currentState => {
+      return {
+        chores: currentState.chores.concat([
+          {
+            chore: tempChore.chore,
+            person: tempChore.person, 
+            days: tempChore.days, 
+            complete: tempChore.complete
+          }
+        ]),
+        tempChore: {
+          chore: "",
+          person: "",
+          days: "",
+          complete: false
+        }
+      };
+    });
+    console.log(this.state.chores);
+  }
+
+  updateChore(e) {
+    const value = e.target.value;
+    console.log(value);
+    this.setState({
+      tempChore: {
+        chore: value, 
+        person: this.state.tempChore.person,
+        days: this.state.tempChore.days,
+        complete: this.state.tempChore.complete
+      }
+    });
+    console.log(this.state.tempChore);
+
+  }
+
+  updateDays(e) {
+    const value = e.target.value;
+    console.log(value);
+    this.setState({
+      tempChore: {
+        chore: this.state.tempChore.chore, 
+        person: this.state.tempChore.person,
+        days: value,
+        complete: this.state.tempChore.complete
+      }
+    });
+    console.log(this.state.tempChore);
+
+  }
+
+  updatePerson(e) {
+    const value = e.target.value;
+    this.setState({
+      tempChore: {
+        chore: this.state.tempChore.chore, 
+        person: value,
+        days: this.state.tempChore.days,
+        complete: this.state.tempChore.complete
+      }
+    });
+    console.log(this.state.tempChore);
+
+  }
+
+  handleDisableClick = e => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
 
   render() {
     return (
       <div>
-        <div class='section-title'>Your Chores</div>
+        <div className='section-title'>Your Chores </div>
         <Card className='chore-card' style={{marginTop: '5%'}}>
           <Table hover borderless className="chore-table">
             <thead>
@@ -21,28 +160,34 @@ export default class ChoreList extends Component {
                 <th>Completed</th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td>Dishes</td>
-                <td>3 Days</td>
-                <td>
-                  <button className="chore-button" type="button" class="btn btn-default btn-circle"><i class="fa fa-check"></i></button>
-                </td>
-              </tr>
-              <tr>
-                <td>Trash</td>
-                <td>3 Days</td>
-                <td>
-                  <button className="chore-button" type="button" class="btn btn-default btn-circle"><i class="fa fa-check"></i></button>
-                </td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan="2">Larry the Bird</td>
-              </tr>
+            <tbody
+            >{this.state.chores
+              .map(item => {
+              return (
+                <tr>
+                  <td>{item.chore}</td>
+                  <td>{item.days} days</td>
+                  <td>
+                    <button className="complete-button" type="button" class="btn btn-default btn-circle" onClick={() => this.click(item)}></button>
+                  </td>
+                </tr>
+                )   
+            })}
             </tbody>
           </Table>
         </Card>
+        <button onClick={this.showAddChoreModal} className="chore-button">
+          Add Chore
+        </button>
+        <ChoreAddItemModal
+          onClose={this.showAddChoreModal}
+          show={this.state.addChoreModal}
+          handleAddChore={this.handleAddChore}
+          tempChore={this.state.tempChore}
+          updateChore={this.updateChore}
+          updateDays={this.updateDays}
+          updatePerson={this.updatePerson}
+        />
       </div>
     );
   }
