a040b2e1e03c07fc00e7405afbf12fb44e70d9d2
Joseph Kim <jhk.joseph@gmail.com>
2019-11-26 20:24:49 -0800

    remove reactstrap

--- frontend/src/components/Chores.jsx
+++ frontend/src/components/Chores.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import NavbarComponent from "./NavbarComponent";
-import { Container, Row, Col, Button } from "reactstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import ChoreList from "./ChoreList";
 import RoomieChoreList from "./RoomieChoreList";
 
-
 function Chores() {
   return (
-    <div style={{height:'100vh'}}>
+    <div style={{ height: "100vh" }}>
       <NavbarComponent />
       <Container>
-        <Row style={{height:'100%', width:'100%'}}>
-          <Col md="6" style={{height:'50%', marginBottom: '10%'}}>
+        <Row style={{ height: "100%", width: "100%" }}>
+          <Col md="6" style={{ height: "50%", marginBottom: "10%" }}>
             <ChoreList />
           </Col>
-          <Col md="6" style={{height:'50%', marginBottom: '10%', width:'100%'}}>
+          <Col
+            md="6"
+            style={{ height: "50%", marginBottom: "10%", width: "100%" }}
+          >
             <RoomieChoreList />
           </Col>
         </Row>
@@ -24,6 +26,3 @@ function Chores() {
 }
 
 export default Chores;
-
-
-
