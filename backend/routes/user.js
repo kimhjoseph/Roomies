6531acd8a39c49bd8a4c1d8b56683263c56bf748
al-bose <36972360+al-bose@users.noreply.github.com>
2019-11-29 15:53:11 -0800

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const ObjectId = mongoose.Types.ObjectId;
 const mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 const User = require('../models/User');
