c7df20d5cbdc1ab64619cd1e423e67565d331123
al-bose <36972360+al-bose@users.noreply.github.com>
2019-12-02 04:06:18 -0800

    Added documentation and cleaned up file

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,24 @@
+/** Express router providing user related routes
+ * @module routes/user
+ * @requires express
+ */
+
+/**
+ * express module
+ * @const
+ */
+
 const express = require("express");
+
+
+
+/**
+ * Express router to mount user related functions on.
+ * @type {object}
+ * @const
+ * @namespace userRouter
+ */
+
 const router = express.Router();
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
@@ -6,37 +26,49 @@ const User = require("../models/User");
 const Apartment = require("../models/Apartment");
 var bcrypt = require("bcryptjs");
 
-//
-// edit_info (post)
-// get_users (get)
+
+
 
 /**
- * Edits user information.
- *
- * Use axios.post(.../user/edit_info, newUser)
- *
- * @param req contains the newUser object which contains first_name, last_name, email, status
- * @return            "Success"
+ * Route for user signup.
+ * @name post/signup
+ * @function
+ * @memberof module:routes/user~userRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @return {string} "Success"
  */
 
-router.post("/signup", async function(req, res) {
-  let user;
-  try {
-    const salt = bcrypt.genSaltSync();
-    req.body.password = bcrypt.hashSync(req.body.password, salt);
-    user = await User.create({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      password: req.body.password,
-      picture: new ObjectId("5de4cfbf7101f652deee8410")
-    });
-  } catch (err) {
-    res.status(400).send(err);
-  }
-  req.session.user = user;
-  res.status(201).send("Success");
+router.post('/signup', async function(req, res) {
+    let user;
+    try {
+        const salt = bcrypt.genSaltSync();
+        req.body.password = bcrypt.hashSync(req.body.password, salt);
+        user = await User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: req.body.password, 
+            picture: new ObjectId('5de4cfbf7101f652deee8410')
+        });
+    }
+    catch(err) {res.status(400).send(err);}
+    req.session.user = user;
+    res.status(201).send("Success");
 });
+
+
+/**
+ * Route for user login.
+ * @name post/login
+ * @function
+ * @memberof module:routes/user~userRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @return {string} "Success"
+ */
 
 router.post("/login", async function(req, res) {
   console.log(req.body);
@@ -55,9 +87,33 @@ router.post("/login", async function(req, res) {
   }
 });
 
+
+/**
+ * Route for user logout to clear session variable.
+ * @name post/logout
+ * @function
+ * @memberof module:routes/user~userRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @return {string} "Success"
+ */
+
 router.post("/logout", function(req, res) {
   res.clearCookie("user_sid");
 });
+
+
+/**
+ * Route for editing user information.
+ * @name post/edit_info
+ * @function
+ * @memberof module:routes/user~userRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @return {string} "Success"
+ */
 
 router.post("/edit_info", async function(req, res) {
   let updatedData = {
@@ -70,7 +126,8 @@ router.post("/edit_info", async function(req, res) {
         ? req.body.lastname
         : req.session.user.last_name,
     email: req.body.email != null ? req.body.email : req.session.user.email,
-    status: req.body.status != null ? req.body.status : req.session.user.status
+    status: req.body.status != null ? req.body.status : req.session.user.status,
+    profile_image: req.body.profile_image != null ? req.body.profile_image : "dummy image"
   };
 
   let user;
@@ -89,12 +146,14 @@ router.post("/edit_info", async function(req, res) {
 });
 
 /**
- * Get relevant users.
- *
- * Use axios.get(.../user/get_users, newUser)
- *
- * @param req contains session user variable
- * @return array of matching User objects found
+ * Route for getting all users of an apartment.
+ * @name get/get
+ * @function
+ * @memberof module:routes/user~userRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @return {List<User>} List of users found.
  */
 
 router.get("/get", async function(req, res) {
@@ -109,29 +168,54 @@ router.get("/get", async function(req, res) {
   res.status(200).json(users);
 });
 
+
+/**
+ * Route for getting the current user.
+ * @name get/get_current_user
+ * @function
+ * @memberof module:routes/user~userRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @return {ObjectID} Id of the user.
+ */
+
 router.get("/get_current_user", function(req, res) {
   res.status(200).json(req.session.user);
 });
 
-router.post("/add_image", async function(req, res) {
+
+/**
+ * Route for adding a profile picture.
+ * @name post/add_image
+ * @function
+ * @memberof module:routes/user~userRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @return {User} the updated user object.
+ */
+
+router.post('/add_image', async function(req, res){
   let file;
   let user;
   let id;
   console.log(req.body);
-  User.findOneAndUpdate(
+User.findOneAndUpdate(
     { _id: new ObjectId(req.session.user._id) },
     { picture: new ObjectId(req.body.img_id) },
     { new: true }
   )
-    .then(user => {
-      req.session.user = user;
-      console.log(req.session.user);
-      console.log("Successfully changed picture");
-      res.status(200).json(users);
-    })
-    .catch(err => res.status(400).json("Error: " + err));
+.then(user => {
+    req.session.user = user;
+    console.log(req.session.user);
+    console.log("Successfully changed picture");
+    res.status(200).json(users);
+})
+.catch(err => res.status(400).json("Error: " + err));
 
-  console.log(req.session.user);
-});
+console.log(req.session.user);
+})
 
 module.exports = router;
+
