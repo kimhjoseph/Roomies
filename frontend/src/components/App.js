c6c54a6e70a5e4eb4da5d57704a60e43f653e2a0
Joseph Kim <jhk.joseph@gmail.com>
2019-12-01 17:05:35 -0800

    improved responsiveness on main page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import RoomiesLanding from "./RoomiesLanding";
 import RoomiesHome from "./RoomiesHome";
 import ShoppingList from "./ShoppingList";
-import Settings from "./Settings"
+import Settings from "./Settings";
 import ChoreList from "./ChoreList";
 import SignUp from "./SignUp";
 import Login from "./Login";
-import CreateJoinApartment from "./CreateJoinApartment"
+import CreateJoinApartment from "./CreateJoinApartment";
 import "./App.css";
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <Router>
           <div>
             <Switch>
-            <Route path="/signup">
+              <Route path="/signup">
                 <SignUp />
               </Route>
               <Route path="/login">
@@ -36,7 +36,7 @@ function App() {
                 <RoomiesHome />
               </Route>
               <Route path="/chores">
-                <ChoreList/>
+                <ChoreList />
               </Route>
               <Route path="/">
                 <RoomiesLanding />
